2a11b4dac208324e5281a3177b52ab627f9aa7d3
godson <godsonamadi687@gmail.com>
2024-04-04 09:11:34 +0100

    search

--- src/Admin/AdminPages/Uploads.jsx
+++ src/Admin/AdminPages/Uploads.jsx
@@ -3,27 +3,41 @@ import { useState, useEffect } from "react";
 import AdminDashboard from "../AdminComponents/AdminDashboard";
 import { txtdb } from "../../firebase-config";
 import { collection, getDocs, deleteDoc, doc } from "firebase/firestore";
+import { CiSearch } from "react-icons/ci";
 
 function Uploads() {
   const [data, setData] = useState([]);
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredData, setFilteredData] = useState([]);
 
   const getData = async () => {
     const valRef = collection(txtdb, "txtData");
     const dataDb = await getDocs(valRef);
     const allData = dataDb.docs.map((val) => ({ ...val.data(), id: val.id }));
     setData(allData);
+    setFilteredData(allData);
   };
   //
 
   const deleteItem = async (itemId) => {
     try {
       await deleteDoc(doc(txtdb, "txtData", itemId));
-      setData(data.filter((item) => item.id !== itemId)); // Update state after deletion
+      setData(data.filter((item) => item.id !== itemId));
+      setFilteredData(filteredData.filter((item) => item.id !== itemId)); // Update filteredData after deletion
     } catch (error) {
       console.error("Error deleting document: ", error);
     }
   };
-
+  //
+  const handleSearchClick = () => {
+    const filtered = data.filter(
+      (value) =>
+        value.txtVal &&
+        value.txtVal.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredData(filtered);
+  };
   //
   useEffect(() => {
     getData();
@@ -34,22 +48,54 @@ function Uploads() {
       <AdminDashboard />
 
       <div className="adminUploads adminContent">
-        <h2>uploads</h2>
+        <h2 className="admin-current-page">Uploads</h2>
+
+        <div className="search-container">
+          <span>
+            <CiSearch className="search-icon" />
+            <input
+              className="searchInput"
+              type="text"
+              placeholder="search for an upload..."
+              onChange={(event) => {
+                setSearchTerm(event.target.value);
+              }}
+            />
+          </span>
+
+          <button onClick={handleSearchClick}>Search</button>
+        </div>
+
+        <div className="categories-container">
+          <h3 className="categories-header">
+            Categories
+          </h3>
+          <div className="categories">
+
+          </div>
+        </div>
 
         <div className="uploads-container">
-          {data.map((value) => (
-            <div key={value.id}>
+          {filteredData.map((value) => (
+            <div className="product" key={value.id}>
               <img
                 src={value.imgUrl}
                 height="200px"
                 width="200px"
                 alt="product"
               />
-              <h2>{value.txtVal}</h2>
-              <p className="product-desciption">{value.desc}</p>
-              <h5 className="product-price">{value.price}</h5>
-              <p>{value.category}</p>
-              <button onClick={() => deleteItem(value.id)}>Delete</button>
+
+              <div className="product-info">
+                <h2 className="product-name">{value.txtVal}</h2>
+
+                <p className="product-description">{value.desc}</p>
+
+                <p className="prodiuct-category">{value.category}</p>
+                <span>
+                  <p className="product-price"> &#8358;{value.price}</p>
+                  <button onClick={() => deleteItem(value.id)}>Delete</button>
+                </span>
+              </div>
             </div>
           ))}
         </div>
@@ -59,3 +105,15 @@ function Uploads() {
 }
 
 export default Uploads;
+
+// .filter((value) => {
+//   if (searchTerm.trim() === " ") {
+//     return true;
+//   } else if (
+//     value.txtVal && value.txtVal.toLowerCase().includes(searchTerm.toLowerCase())
+//   ) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// })
