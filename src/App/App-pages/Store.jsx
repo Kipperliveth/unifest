866edc8d320f2e70fbe08a1a7f27001280cf9e0b
godson <godsonamadi687@gmail.com>
2024-04-06 02:24:49 +0100

    almost done

--- src/App/App-pages/Store.jsx
+++ src/App/App-pages/Store.jsx
@@ -3,13 +3,42 @@ import React from "react";
 import UserNav from "../App-components/UserNav";
 // import { storage } from "../../firebase-config";
 import { ref, listAll, getDownloadURL } from "firebase/storage";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, deleteDoc, doc } from "firebase/firestore";
 import { txtdb } from "../../firebase-config";
+import { CiSearch } from "react-icons/ci";
+import all from "../../stock/allmain.png";
+import sitting from "../../stock/couchicon.png";
+import curtains from "../../stock/curtainicon.png";
+import room from "../../stock/roomicon.png";
+import lights from "../../stock/lighticon.png";
+import tables from "../../stock/tableicon.png";
+import storageicon from "../../stock/storageicon.png";
 
 function Store() {
   const [imageList, setImageList] = useState([]);
 
+  const [isLoading, setIsLoading] = useState(true);
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredData, setFilteredData] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
   const [data, setData] = useState([]);
+
+  //pop
+
+  const [isProductModalOpen, setIsProductModalOpen] = useState(false);
+  const [selectedProductData, setSelectedProductData] = useState(null);
+
+  const handleProductClick = (productData) => {
+    setIsProductModalOpen(true);
+    setSelectedProductData(productData); // Store clicked product data
+  };
+
+  const handleCloseModal = () => {
+    setIsProductModalOpen(false);
+    setSelectedProductData(null); // Clear selected product on close
+  };
 
   //get data
 
@@ -18,46 +47,213 @@ function Store() {
     const dataDb = await getDocs(valRef);
     const allData = dataDb.docs.map((val) => ({ ...val.data(), id: val.id }));
     setData(allData);
-  };
+    setFilteredData(allData);
+  }; //
 
+  const handleSearchClick = () => {
+    const filtered = data.filter(
+      (value) =>
+        value.txtVal &&
+        value.txtVal.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredData(filtered);
+  };
+  //
+
+  const deleteItem = async (itemId) => {
+    try {
+      await deleteDoc(doc(txtdb, "txtData", itemId));
+      setData(data.filter((item) => item.id !== itemId));
+      setFilteredData(filteredData.filter((item) => item.id !== itemId)); // Update filteredData after deletion
+    } catch (error) {
+      console.error("Error deleting document: ", error);
+    }
+  };
+  //
+
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+    if (category === "All") {
+      setFilteredData(data);
+    } else {
+      const filtered = data.filter((item) => item.category === category);
+      setFilteredData(filtered);
+    }
+  };
+  //
+
+  //display data onload
   useEffect(() => {
     getData();
   }, []);
 
-  // const imageListRef = ref(storage, "images/");
-
-  // useEffect(() => {
-  //   listAll(imageListRef).then((response) => {
-  //     response.items.forEach((item) => {
-  //       getDownloadURL(item).then((url) => {
-  //         setImageList((prev) => [...prev, { url, ref: item }]);
-  //       });
-  //     });
-  //   });
-  // }, []);
+  //skeleton loading
+  useEffect(() => {
+    setIsLoading(true); // Start loading before fetching data
+    getData().then(() => {
+      setIsLoading(false); // Stop loading after data is fetched
+    });
+  }, []);
 
   return (
     <div className="theStore">
       <UserNav />
 
       <div className="uploaded-posts-container">
-        <h1> welcome to the store</h1>
+        <div className="search-container">
+          <span>
+            <CiSearch className="search-icon" />
+            <input
+              className="searchInput"
+              type="text"
+              placeholder="search for an something..."
+              onChange={(event) => {
+                setSearchTerm(event.target.value);
+              }}
+            />
+          </span>
 
-        <div className="uploaded-posts">
-          {data.map((value) => (
-            <div key={value.id}>
-              <img
-                src={value.imgUrl}
-                height="200px"
-                width="200px"
-                alt="product"
-              />
-              <h1>{value.txtVal}</h1>
-              <p className="product-desciption">{value.desc}</p>
-              <h1>{value.price}</h1>
-              <p>{value.category}</p>
+          <button onClick={handleSearchClick}>Search</button>
+        </div>
+
+        <div className="categories-container">
+          <h3 className="categories-header">Categories</h3>
+          <div className="categories">
+            <span className="category-name">
+              <button onClick={() => handleCategoryClick("All")}>
+                <img src={all} alt="" />
+              </button>
+              All
+            </span>
+
+            <span className="category-name">
+              <button onClick={() => handleCategoryClick("Sitting")}>
+                <img src={sitting} alt="" />
+              </button>
+              <p>Sitting</p>
+            </span>
+
+            <span className="category-name">
+              <button onClick={() => handleCategoryClick("Curtains")}>
+                <img src={curtains} alt="" />
+              </button>
+              <p>Curtains</p>
+            </span>
+
+            <span className="category-name">
+              <button onClick={() => handleCategoryClick("Tables")}>
+                <img src={tables} alt="" />
+              </button>
+              <p>Tables</p>
+            </span>
+
+            <span className="category-name">
+              <button onClick={() => handleCategoryClick("Room")}>
+                <img src={room} alt="" />
+              </button>
+              <p>Room</p>
+            </span>
+
+            <span className="category-name">
+              <button onClick={() => handleCategoryClick("Lights")}>
+                <img src={lights} alt="" />
+              </button>
+              <p>Lights</p>
+            </span>
+
+            <span className="category-name">
+              <button onClick={() => handleCategoryClick("Storage")}>
+                <img src={storageicon} alt="" />
+              </button>
+              <p>Storage</p>
+            </span>
+          </div>
+        </div>
+
+        {isLoading ? (
+          <div className="loading-message">
+            <div className="loading-card">
+              <div className="loading-img"></div>
+              <div className="loading-text"></div>
+              <div className="loading-text-II"></div>
             </div>
-          ))}
+
+            <div className="loading-card">
+              <div className="loading-img"></div>
+              <div className="loading-text"></div>
+              <div className="loading-text-II"></div>
+            </div>
+
+            <div className="loading-card">
+              <div className="loading-img"></div>
+              <div className="loading-text"></div>
+              <div className="loading-text-II"></div>
+            </div>
+
+            <div className="loading-card">
+              <div className="loading-img"></div>
+              <div className="loading-text"></div>
+              <div className="loading-text-II"></div>
+            </div>
+
+            <div className="loading-card">
+              <div className="loading-img"></div>
+              <div className="loading-text"></div>
+              <div className="loading-text-II"></div>
+            </div>
+
+            <div className="loading-card">
+              <div className="loading-img"></div>
+              <div className="loading-text"></div>
+              <div className="loading-text-II"></div>
+            </div>
+          </div>
+        ) : (
+          <div className="uploaded-posts">
+            {filteredData.map((value) => (
+              <div
+                className="product"
+                key={value.id}
+                onClick={() => handleProductClick(value)}
+              >
+                <img
+                  src={value.imgUrl}
+                  height="200px"
+                  width="200px"
+                  alt="product"
+                />
+
+                <div className="product-info">
+                  <h2 className="product-name">{value.txtVal}</h2>
+
+                  <p className="product-description">{value.desc}</p>
+
+                  <p className="product-category">{value.category}</p>
+                  <span>
+                    <p className="product-price"> &#8358;{value.price}</p>
+                    <button onClick={() => deleteItem(value.id)}>
+                      Add to Cart
+                    </button>
+                  </span>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+
+        <div className={`product-modal ${isProductModalOpen ? "open" : ""}`}>
+          {selectedProductData && (
+            <div>
+              <h2>{selectedProductData.txtVal}</h2>
+              <img src={selectedProductData.imgUrl} alt="Product" />{" "}
+              {/* Add image */}
+              <p>{selectedProductData.desc}</p>
+              <p>Category: {selectedProductData.category}</p>
+              <p>Price: &#8358;{selectedProductData.price}</p>
+              {/* ... other product details ... */}
+            </div>
+          )}
+          <button onClick={handleCloseModal}>Close</button>
         </div>
       </div>
     </div>
