1cb1e814adfe4c967882df90369103ec2eefc2b3
kipperliveth <godsonamadi687@gmail.com>
2025-03-26 11:50:50 +0100

    done

--- src/App/App-pages/MasterclassMain.jsx
+++ src/App/App-pages/MasterclassMain.jsx
@@ -52,8 +52,8 @@ function MasterclassMain() {
   };
   
   const itemVariants = {
-    hidden: { opacity: 0, y: 50 },
-    visible: { opacity: 1, y: 0, transition: { duration: 0.8, ease: "easeOut" } },
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { duration: 0.8, ease: "easeOut" } },
   };
   
 
