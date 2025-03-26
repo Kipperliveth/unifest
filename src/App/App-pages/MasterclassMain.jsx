8616c29813413c3908abd8ddfa2d1c4e4b3713e4
kipperliveth <godsonamadi687@gmail.com>
2025-03-26 12:08:49 +0100

    snapchat

--- src/App/App-pages/MasterclassMain.jsx
+++ src/App/App-pages/MasterclassMain.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { FaInstagram } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
+import { FaSnapchat } from "react-icons/fa6";
 import { IoLogoTiktok } from "react-icons/io5";
 import { MdMailOutline } from "react-icons/md";
 import { txtdb } from "../../firebase-config";
@@ -90,7 +91,7 @@ function MasterclassMain() {
 
   <motion.div className="form-socials" variants={itemVariants}>
     <a href="https://www.instagram.com/unifest001?igsh=Y3UzY3U3Ym44dmh6"><FaInstagram className="icon" /></a>
-    <a href="https://x.com/unifest001?t=v1LY_RCY5_DHDN7XBiMzqA&s=09"><FaXTwitter className="icon" /></a>
+    <a href="https://www.snapchat.com/add/sobu_thevybez?share_id=6Yw7qQyiEtA&locale=en-GB"><FaSnapchat className="icon" /></a>
     <a href="mailto:unifest12@gmail.com?subject=Unifest%20'24%20Inquiry"><MdMailOutline className="icon" /></a>
     <a href="https://www.tiktok.com/@unifest001?_t=8nv0OSN4tUL&_r=1"><IoLogoTiktok className="icon" /></a>
   </motion.div>
